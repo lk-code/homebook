16c502c6da5eebbe8a29190c59372b4f4ffc48f7
lk-code <1656571+lk-code@users.noreply.github.com>
2025-08-03 01:10:11 +0200

    HB-6: add client registration in frontend

--- source/HomeBook.Frontend/Program.cs
+++ source/HomeBook.Frontend/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HomeBook.Frontend;
+using HomeBook.Frontend.Services.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,8 @@ builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
+
+builder.Services.AddFrontendServices(builder.Configuration)
+    .AddBackendClient(builder.Configuration);
 
 await builder.Build().RunAsync();
