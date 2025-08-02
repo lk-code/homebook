c14622043c43580782b2c38789dd33e6d271f8a6
lk-code <1656571+lk-code@users.noreply.github.com>
2025-08-02 13:38:08 +0200

    add frontend mudblazor

--- source/WebApp/Program.cs
+++ source/WebApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using de.keysoftware.HomeBook.WebApp;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,6 +11,8 @@ builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
+
+builder.Services.AddMudServices();
 
 builder.Services.AddOidcAuthentication(options =>
 {
