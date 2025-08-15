2ab6e80e95bba43d5bdabe622fe7f6bd7cb51e07
lk-code <1656571+lk-code@users.noreply.github.com>
2025-08-16 00:48:37 +0200

    HB-17: fix theme colors

--- source/HomeBook.Frontend/Themes/HomebookTheme.cs
+++ source/HomeBook.Frontend/Themes/HomebookTheme.cs
@@ -1,25 +1,35 @@
 using MudBlazor;
+using MudBlazor.Utilities;
 
 namespace HomeBook.Frontend.Themes;
 
+
 public class HomebookTheme : MudTheme
 {
+    // https://www.color-hex.com/color-palette/1063036
+    private static MudColor Primary { get; set; } = "#382960";
+    private static MudColor Secondary { get; set; } = "#13bd85";
+    private static MudColor Tertiary { get; set; } = "#373f31";
+    private static MudColor TextPrimary { get; set; } = "#080606";
+    private static MudColor TextSecondary { get; set; } = TextPrimary.ColorLighten(0.4);
+
     private static readonly MudTheme _theme = new()
     {
         LayoutProperties = new LayoutProperties()
         {
             DefaultBorderRadius = "20px"
         },
-        // https://www.color-hex.com/color-palette/1063036
         PaletteLight = new PaletteLight()
         {
-            Primary = "#382960",
-            TextPrimary = "#FFFFFF",
-
-            Secondary = "#13bd85",
-            TextSecondary = "#2a0606",
+            Primary = Primary,
+            Secondary = Secondary,
+            Tertiary = Tertiary,
 
             Background = "#F5F5F5",
+
+            TextPrimary = TextPrimary,
+            TextSecondary = TextSecondary,
+
 
             AppbarBackground = "#382960",
             AppbarText = "#000000",
