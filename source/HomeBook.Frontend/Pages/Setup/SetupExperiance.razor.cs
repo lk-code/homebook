ae9b9d818d99162a141bd11a37d3e721e95fbdec
lk-code <1656571+lk-code@users.noreply.github.com>
2025-08-16 00:23:17 +0200

    HB-17: add footer informations

--- source/HomeBook.Frontend/Pages/Setup/SetupExperiance.razor.cs
+++ source/HomeBook.Frontend/Pages/Setup/SetupExperiance.razor.cs
@@ -4,11 +4,18 @@ namespace HomeBook.Frontend.Pages.Setup;
 
 public partial class SetupExperiance : ComponentBase
 {
+    private string _appVersion = string.Empty;
+    private string _appServer = string.Empty;
+
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         await base.OnAfterRenderAsync(firstRender);
 
         if (!firstRender)
             return;
+
+        _appVersion = Configuration["Version"] ?? "Unknown";
+        _appServer = Configuration["Backend:Host"] ?? "Unknown";
+        StateHasChanged();
     }
 }
