15808df143be8a92b3b55f9867382ab0e5a9cf5d
lk-code <1656571+lk-code@users.noreply.github.com>
2026-03-25 15:01:12 +0100

    HB-294 - Update NavMenu links and navigation redirection

--- source/HomeBook.Frontend.UI/Resources/LocalizationStrings.Designer.cs
+++ source/HomeBook.Frontend.UI/Resources/LocalizationStrings.Designer.cs
@@ -1520,5 +1520,11 @@ namespace HomeBook.Frontend.UI.Resources {
                 return ResourceManager.GetString("Settings_Developer_ConfigurationItemsBackend_Title", resourceCulture);
             }
         }
+        
+        public static string Settings_NavMenu_Appearance_Text {
+            get {
+                return ResourceManager.GetString("Settings_NavMenu_Appearance_Text", resourceCulture);
+            }
+        }
     }
 }
